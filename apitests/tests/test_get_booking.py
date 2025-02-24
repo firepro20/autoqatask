677b98ceb1b445f28a40bd5cb8360a976e7e4fa7
firepro20 <firepro20@gmail.com>
2025-02-24 23:12:41 +0100

    first test

---
+++ apitests/tests/test_get_booking.py
@@ -0,0 +1,8 @@
+from apitests.utils.api_client import get_booking
+
+def test_get_booking():
+    booking_id = 1
+    response = get_booking(booking_id)
+    
+    assert response.status_code == 200, "Booking retrieval failed!"
+    assert "firstname" in response.json(), "Missing firstname in response"
